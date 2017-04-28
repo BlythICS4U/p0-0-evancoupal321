04d6421d3cbe7c696ccce634d026f7a0ecae1789
Chris Gallinaro <cgallinaro@ea.com>
2017-04-27 22:39:14 -0400

    Adding empty NetBeans project

---
+++ MathFormulas/src/mathformulas/MathFormulas.java
@@ -0,0 +1,16 @@
+package mathformulas;
+
+/**
+ *
+ * @author cgallinaro
+ */
+public class MathFormulas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Put your math formula implementations here
+    }
+    
+}
