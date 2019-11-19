e14eac248c78a6ee3e20594481ff3551891d2284
evancoupal321 <57766866+evancoupal321@users.noreply.github.com>
2019-11-19 12:14:30 -0500

    Added formulas

--- MathFormulas/src/mathformulas/MathFormulas.java
+++ MathFormulas/src/mathformulas/MathFormulas.java
@@ -10,7 +10,23 @@ public class MathFormulas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Put your math formula implementations here
+        double v, w, x, y, z;
+        int a = 1;
+        int b = 2;
+        int m = 3;
+        x = (a - 5)*(b + 4);
+        y = m*x + b;
+        z = 4*((2*x + 7)*(3*x - 2));
+        w = (m - 4*x)/(7-y);
+        v = (a*Math.pow(x, 2) + b*x + z)/(m*x - w);
+        System.out.println("A is " + a);
+        System.out.println("B is " + b);
+        System.out.println("M is " + m);
+        System.out.println("X is " + x);
+        System.out.println("Y is " + y);
+        System.out.println("Z is " + z);
+        System.out.println("W is " + w);
+        System.out.println("V is " + v);
     }
     
 }
