23bab523ad44c7257ae639bb96463180c076ed87
evancoupal321 <57766866+evancoupal321@users.noreply.github.com>
2019-11-21 12:04:08 -0500

    Update MathFormulas.java

--- MathFormulas/src/mathformulas/MathFormulas.java
+++ MathFormulas/src/mathformulas/MathFormulas.java
@@ -11,9 +11,9 @@ public class MathFormulas {
      */
     public static void main(String[] args) {
         double v, w, x, y, z;
-        int a = 1;
-        int b = 2;
-        int m = 3;
+        double a = 1;
+        double b = 2;
+        double m = 3;
         x = (a - 5)*(b + 4);
         y = m*x + b;
         z = 4*((2*x + 7)*(3*x - 2));
